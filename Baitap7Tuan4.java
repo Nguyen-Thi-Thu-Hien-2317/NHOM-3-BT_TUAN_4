dc6a22e135186911386d17332029be04f8c8b12f
Nguyen-Thi-Thu-Hien-2317 <thuhien3176@gmail.com>
2025-09-18 16:46:09 +0700

    Update Baitap7Tuan4.java

--- Baitap7Tuan4.java
+++ Baitap7Tuan4.java
@@ -4,7 +4,7 @@ public class Baitap7Tuan4 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Nhap so phan tu cua mang: ");
-        int n = scanner.nextInt();s
+        int n = scanner.nextInt();
         int[] numbers = new int[n];
         System.out.println("Nhap cac phan tu cua mang:");
         for (int i = 0; i < n; i++) {
@@ -21,4 +21,5 @@ public class Baitap7Tuan4 {
 
         scanner.close();
     }
+
 }
